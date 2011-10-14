bcf24b2af1294d16584912bc25b9f3e894b8f977
Martin Ongsiek <martin.ongsiek@gmail.com>
2011-10-14 15:25:37 +0200

    Angefangen mathematische Gundrechenarten vom avr miteinander zu vergleichen.

--- Makefile
+++ Makefile
@@ -5,18 +5,21 @@ OPTIMIZE  = -Os
 WARNINGS  =
 LIBS      = 
 SIM_OBJ   = $(patsubst %.o,%_sim.o,$(OBJ))
-SIM_DEV   = atmega48
-SIM_DEFS  = -DSIMULAVR 
+SIM_DEV   = atmega128
+SIM_WIO   = 0x30
+SIM_RIO   = 0x0E
+SIM_DEFS  = -DSIMULAVR -DSIM_WIO=$(SIM_WIO)
 SIM_FLAGS   = -MMD -g -mmcu=$(SIM_DEV) $(OPTIMIZE) $(WARNINGS) $(SIM_DEFS)
 SIM_ASFLAGS = 
 SIM_LDFLAGS = -Wl,-Map,$(OUT)_sim.map
+
 PC_OBJ    = 
 
 MCU_CC    = avr-gcc
 OBJDUMP   = avr-objdump
 
 simul: $(OUT)_sim.elf $(OUT)_sim.lst 
-	simulavr -d $(SIM_DEV) -f $(OUT)_sim.elf -T exit
+	simulavr -d $(SIM_DEV) -W $(SIM_WIO),- -f $(OUT)_sim.elf -T exit
 
 %_sim.o: %.c
 	$(MCU_CC) $(SIM_FLAGS) -o $@ -c $<
@@ -29,6 +32,8 @@ simul: $(OUT)_sim.elf $(OUT)_sim.lst
 
 all:
 	echo make simul   for avr simulation
+
+$(SIM_OBJ):Makefile
 
 $(OUT)_sim.elf: $(SIM_OBJ)
 	$(MCU_CC) $(SIM_FLAGS) $(SIM_LDFLAGS) -o $@ $^ $(LIBS)
--- test.c
+++ test.c
@@ -1,10 +1,13 @@
+#ifdef  __IAR_SYSTEMS_ICC__
+#include <ioavr.h>
+#else
 #include <avr/io.h>
+#endif
 
 typedef void (*test_func_t)(void);
 
-
-float a = 12.0, b = 12232.4, c = 0.0;
-short d = 323;
+float a = 3454.345334, b = 12232.45345, c = 0.0;
+short d = 23442;
 
 void float_convert_to_float() {
 	c = d;
@@ -36,8 +39,135 @@ void float_div_const(void) {
 
 void float_div(void) {
 	c = a / b;
+}
+
+unsigned short i_a = 23444, i_b = 23, i_c;
+
+static inline unsigned short div10(unsigned short in) { 
+    return (((unsigned long) in * 52429L)) >> 19;
+}
+
+void int16_div10() {
+	i_c = i_a / 10;
+}
+
+void int16_div10_fast() {
+	i_c = div10(i_a);
+}
+
+void int16_div16() {
+	i_c = i_a / 16;
+}
+
+void int16_div() {
+    i_c = i_a / i_b;
 } 
 
+void int16_mult_const() {
+    i_c = i_a * 234;
+} 
+
+void int16_mult() {
+    i_c = i_a * i_b;
+} 
+
+unsigned long l_a = 23444, l_b = 23, l_c;
+
+void int32_div10() {
+	l_c = l_a / 10;
+}
+
+void int32_div16() {
+	l_c = l_a / 16;
+}
+
+void int32_div() {
+    l_c = l_a / l_b;
+} 
+
+void int32_mult_const() {
+    l_c = l_a * 234;
+} 
+
+void int32_mult() {
+    l_c = l_a * l_b;
+}
+
+
+#ifdef  __IAR_SYSTEMS_ICC__
+/* This port correponds to the "-W 0x20,-" command line option. */
+#define special_output_port (*((__io *) SIM_WIO))
+/* This port correponds to the "-R 0x22,-" command line option. */
+#define special_input_port  (*((__io *) SIM_RIO))
+#else
+/* This port correponds to the "-W 0x20,-" command line option. */
+#define special_output_port (*((volatile char *) SIM_WIO))
+/* This port correponds to the "-R 0x22,-" command line option. */
+#define special_input_port  (*((volatile char *) SIM_RIO))
+#endif
+
+static PrintString(const char* str) {
+	const char *c;
+	for(c = str; *c; c++)
+	  special_output_port = *c;
+}
+
+static void PrintSignedShortFormated(signed short value) {
+    unsigned char sBuf0[7];
+    unsigned char *sBuf = &sBuf0[6];
+    unsigned char  minus;
+    unsigned short valueAlt;   // Alte Wert für schneller % 10 Berechnung
+    unsigned short valueCalc;  // muss unsigned weil signed short kann kein 32768
+    
+    *sBuf = 0;       // String Terminierung
+    if (value < 0) { // Vorzeichen behandeln
+      minus = '-';
+      valueCalc = -value;
+    } else {
+      minus = ' ';
+      valueCalc = value;
+    }
+    if (!valueCalc) 
+      *--sBuf = '0';
+    else while (valueCalc) {
+      valueAlt = valueCalc;
+      //  schneller als valueCalc /= 10;
+      valueCalc = div10(valueCalc); 
+      // schneller als  *--sBuf = '0' + (valueAlt % 10)
+      *--sBuf = '0' + valueAlt - (valueCalc * 10); 
+    } 
+    *--sBuf = minus;                  // Minuszeichen vor die höchstwertigste Stelle setzen
+    while (sBuf > sBuf0)              // mit Leerzeichen auffüllen (für Formatierung)  
+      *--sBuf = ' ';
+    PrintString((const char*) sBuf0); // String ausgeben
+}
+
+/*
+static void PrintSignedShort(signed short value) {
+    unsigned char sBuf0[7];
+    unsigned char *sBuf = &sBuf0[6];
+    unsigned char x = 6, minus;
+    unsigned short valueAlt;   // Alte Wert für schneller % 10 Berechnung
+    unsigned short valueCalc;  // muss unsigned weil signed short kann kein 32768
+    
+    *sBuf = 0;
+    if (value < 0) { // Vorzeichen behandeln
+      minus = 1;
+      valueCalc = -value;
+    } else {
+      minus = 0;
+      valueCalc = value;
+    }
+    do {
+      valueAlt = valueCalc;
+      valueCalc = div10(valueCalc); //  schneller als valueCalc /= 10;
+      *--sBuf = '0' + valueAlt - (valueCalc * 10); // schneller als  *--sBuf = '0' + (valueAlt % 10)
+    } while (valueCalc);
+    if (minus)
+		*--sBuf = '-';                // Minuszeichen vor die höchstwertigste Stelle setzen
+    PrintString((const char*) sBuf0); // String ausgeben
+}
+*/
 
 #define TIME_FUNC(func) print_execute_time(func, "" # func) 
 
@@ -46,14 +176,23 @@ unsigned short print_execute_time(test_func_t function, char name[]) {
 	TCCR1A = 0;
 	TCCR1B = 0;
 	TCNT1  = 0;
+	TIFR   = 0;
 	TCCR1B = 1; // prescaler 1 Timer starten
 	function();
 	TCCR1B = 0; // Timer stoppen
 	// print
+	PrintSignedShortFormated(TCNT1);
+	PrintString(" = ");
+	PrintString(name);
+	if (TIFR & (1 << 2))
+		PrintString(" (Timeroverflow!)");	
+	PrintString("\n");
 	return TCNT1;
 }
 
 int main() {
+	PrintString("\nZeitmessung Rechenoperationen\n");
+	PrintString("\n Takte   Funktionsname\n");
 	TIME_FUNC(float_convert_to_float);
 	TIME_FUNC(float_convert_to_int);
 	TIME_FUNC(float_add_const);
@@ -61,6 +200,17 @@ int main() {
 	TIME_FUNC(float_mul_const);
 	TIME_FUNC(float_mul);
 	TIME_FUNC(float_div_const);
-	TIME_FUNC(float_div);	
+	TIME_FUNC(float_div);
+	TIME_FUNC(int16_div10);
+	TIME_FUNC(int16_div10_fast);
+	TIME_FUNC(int16_div16);
+	TIME_FUNC(int16_div);
+	TIME_FUNC(int16_mult_const);
+	TIME_FUNC(int16_mult);	
+	TIME_FUNC(int32_div10);
+	TIME_FUNC(int32_div16);
+	TIME_FUNC(int32_div);
+	TIME_FUNC(int32_mult_const);
+	TIME_FUNC(int32_mult);		
 	return 0;	
 }
