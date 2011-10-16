7a956c34283988c2ef7a77228cb5a1ec775f4723
Martin Ongsiek <martin.ongsiek@gmail.com>
2011-10-16 23:17:41 +0200

    added integer implementations

---
+++ asmlib.h
@@ -0,0 +1,11 @@
+#ifndef ASM_LIB_H
+#define ASM_LIB_H
+
+#include <stdint.h>
+
+uint16_t isqrt32(uint32_t n);
+uint8_t  isqrt16(uint16_t n);
+uint16_t ihypot(int16_t x, int16_t y);
+
+
+#endif // ASM_LIB_H
